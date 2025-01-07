9c26a6a378acbeb542de3484d3a0219720155fdb
RyanS <RyanS@DESKTOP-A1LOUJQ>
2025-01-07 00:26:40 +0000

    qrcode

--- src/main/java/ataque/arena/espacesistem/QRcode.java
+++ src/main/java/ataque/arena/espacesistem/QRcode.java
@@ -6,13 +6,8 @@ package ataque.arena.espacesistem;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.Random;
 
 /**
