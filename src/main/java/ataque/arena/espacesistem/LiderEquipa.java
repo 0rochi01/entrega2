e816777bd14415c6676c365d62dc4a154fa38d44
RyanS <RyanS@DESKTOP-A1LOUJQ>
2025-01-07 09:21:14 +0000

    espectador ja se inscreve e lista eventos inscritos gerando o qrcode

---
+++ src/main/java/ataque/arena/espacesistem/LiderEquipa.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ataque.arena.espacesistem;
+
+/**
+ *
+ * @author RyanS
+ */
+public class LiderEquipa extends Utilizador {
+    
+    public LiderEquipa(String nomeCompleto, String nomeDeUtilizador, String email, String password, String privilegio, Criptografia criptografia) {
+        super(nomeCompleto, nomeDeUtilizador, email, password, privilegio, criptografia);
+    }
+    
+}
